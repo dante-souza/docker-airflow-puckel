df7729ff47db6f69aa7a250bd6244db06fbae79e
dante.souza <dante.souza@viuavarejo.com.br>
2024-01-30 16:14:05 -0300

    arrumando o import do modulo

--- dags/D01_my_first_databricks_dag_jobcluster.py
+++ dags/D01_my_first_databricks_dag_jobcluster.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
 from airflow.utils.dates import days_ago
 from airflow import DAG
 from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator
